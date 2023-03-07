9cd995b0d033f4d0a628eb7b256f4592554ecab9
Checcolino1312 <75864171+Checcolino1312@users.noreply.github.com>
2023-03-07 20:06:45 +0100

    risoluzione registrazione caregiver + sql

--- models/Caregiver.php
+++ models/Caregiver.php
@@ -3,6 +3,8 @@
 namespace app\models;
 
 use Yii;
+use yii\base\NotSupportedException;
+use yii\web\IdentityInterface;
 
 /**
  * This is the model class for table "caregiver".
@@ -15,11 +17,18 @@ use Yii;
  * @property string $cellulare
  * @property int $id_logopedista
  */
-class Caregiver extends \yii\db\ActiveRecord
+class Caregiver extends \yii\db\ActiveRecord implements IdentityInterface
 {
+    const STATUS_ACTIVE = 2;
     /**
      * {@inheritdoc}
      */
+
+    public function getId()
+    {
+        return $this->getPrimaryKey();
+    }
+
     public static function tableName()
     {
         return 'caregiver';
@@ -52,4 +61,58 @@ class Caregiver extends \yii\db\ActiveRecord
             'id_logopedista' => 'Id Logopedista',
         ];
     }
+
+    public function beforeSave($insert)
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->password = Yii::$app->security->generatePasswordHash($this->password);
+
+                $this->generateAuthKey(); // aggiungi questa riga
+            }
+            return true;
+        }
+        return false;
+    }
+    public function validatePassword($password)
+    {
+        return Yii::$app->security->validatePassword($password, $this->password);
+    }
+    public function setPassword($password)
+    {
+        $this->password_hash = Yii::$app->security->generatePasswordHash($password);
+    }
+
+    /**
+     * Generates "remember me" authentication key
+     */
+    public function generateAuthKey()
+    {
+        $this->auth_key = Yii::$app->security->generateRandomString();
+    }
+
+    public function validateAuthKey($authKey)
+    {
+        return $this->getAuthKey() === $authKey;
+    }
+
+    public function getAuthKey()
+    {
+        return $this->auth_key;
+    }
+
+    public static function findIdentity($id)
+    {
+        return static::findOne(['id' => $id, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+    }
+
+
 }
